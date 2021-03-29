982d780a682d45401f612c6132c911b6545fb2a7
hYdos <haydenv06@gmail.com>
2021-03-29 18:13:50 +1000

    new design based on Valoghese's idea

--- src/main/kotlin/me/hydos/rosella/editor/Editor.kt
+++ src/main/kotlin/me/hydos/rosella/editor/Editor.kt
@@ -4,7 +4,6 @@ import me.hydos.rosella.Rosella
 import me.hydos.rosella.render.io.Window
 import me.hydos.rosella.render.material.Material
 import me.hydos.rosella.render.model.ColourGuiRenderObject
-import me.hydos.rosella.render.model.GuiRenderObject
 import me.hydos.rosella.render.resource.Global
 import me.hydos.rosella.render.resource.Identifier
 import me.hydos.rosella.render.resource.Resource
@@ -79,48 +78,57 @@ object Editor {
 		)
 		rosella.reloadMaterials()
 
-		// Left Panel
+		addSidePanel(1.63f, -0.33f) // Left Panel
+		addWidePanel(-0.133f, 1f) // Bottom Panel
+
+//		// Render Folders
+//		var y = 0
+//		while (y <= 2) {
+//			var x = 0
+//			while (x <= 13) {
+//				addFolder(-(7.4f + 1.33f * x), 2.88f + y * 1.6f)
+//				x++
+//			}
+//			y++
+//		}
+	}
+
+//	private fun addFolder(x: Float, y: Float) {
+//		rosella.addRenderObject(
+//			GuiRenderObject(
+//				folder,
+//				-0.8f
+//			).apply {
+//				scale(0.03f, 0.02f)
+//				translate(
+//					0.75f / rosella.getWidth() + x,
+//					0.75f / rosella.getHeight() + y
+//				)
+//			}
+//		)
+//	}
+
+	private fun addSidePanel(x: Float, y: Float) {
 		rosella.addRenderObject(
 			ColourGuiRenderObject(
 				panel,
-				Vector3f(41 / 255f, 41 / 255f, 41 / 255f)
+				Vector3f(60f / 255, 63f / 255, 65f / 255)
 			).apply {
-				scale(0.3f, 0.5f)
-				translate(2.2f / rosella.getWidth() - 2.2f, 1.5f / rosella.getHeight() - 0.33f)
+				scale(0.36f, rosella.getHeight())
+				translate(2f / rosella.getWidth() + x, 1.5f / rosella.getHeight() + y)
 			}
 		)
+	}
 
-		// Bottom Panel
+	private fun addWidePanel(x: Float, y: Float) {
 		rosella.addRenderObject(
 			ColourGuiRenderObject(
 				panel,
-				Vector3f(51 / 255f, 51 / 255f, 51 / 255f)
+				Vector3f(55f / 255, 58f / 255, 60f / 255)
 			).apply {
-				scale(2f, 0.28f)
-				translate(2.2f / rosella.getWidth() - 0.001f, 1.5f / rosella.getHeight() + 0.8f)
+				scale(1.2f, 0.28f)
+				translate(1.2f / rosella.getWidth() + x, 1.5f / rosella.getHeight() + y)
 			}
 		)
-
-		// Render Folders
-		var y = 0
-		while (y <= 2) {
-			var x = 0
-			while (x <= 13) {
-				rosella.addRenderObject(
-					GuiRenderObject(
-						folder,
-						-0.8f
-					).apply {
-						scale(0.03f, 0.02f)
-						translate(
-							1.5f / rosella.getWidth() - 7.4f + 1.33f * x,
-							1.5f / rosella.getHeight() + 2.88f + y * 1.6f
-						)
-					}
-				)
-				x++
-			}
-			y++
-		}
 	}
 }
